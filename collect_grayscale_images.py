4ee53b613a74bd5bb56651ddd7fc755231986559
thingamabob2038 <spaceage2037@gmail.com>
2026-05-08 10:20:09 -0400

    New files added

--- collect_grayscale_images.py
+++ collect_grayscale_images.py
@@ -1,7 +1,34 @@
 import requests
 from pathlib import Path
 
-out = Path("grayscale_100x100_images_test")
+out = Path("img50x50_test")
+out.mkdir(exist_ok=True)
+
+for i in range(1, 10001):
+    url = f"https://picsum.photos/seed/random_{i}/50/50?grayscale"
+    r = requests.get(url, timeout=30)
+    r.raise_for_status()
+    (out / f"image_{i:05d}.jpg").write_bytes(r.content)
+
+out = Path("img50x50")
+out.mkdir(exist_ok=True)
+
+for i in range(10001, 10101):
+    url = f"https://picsum.photos/seed/random_{i}/50/50?grayscale"
+    r = requests.get(url, timeout=30)
+    r.raise_for_status()
+    (out / f"image_{i:05d}.jpg").write_bytes(r.content)
+
+out = Path("img100x100")
+out.mkdir(exist_ok=True)
+
+for i in range(1, 10001):
+    url = f"https://picsum.photos/seed/random_{i}/100/100?grayscale"
+    r = requests.get(url, timeout=30)
+    r.raise_for_status()
+    (out / f"image_{i:05d}.jpg").write_bytes(r.content)
+
+out = Path("img100x100_test")
 out.mkdir(exist_ok=True)
 
 for i in range(10001, 10101):
